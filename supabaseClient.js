0e1d68bb5bf9b340654acfb4dea5fd1293f2ae8c
anjasmarats <atsakunprogramming@gmail.com>
2025-10-13 10:36:35 +0000

    Initial commit

---
+++ supabaseClient.js
@@ -0,0 +1,12 @@
+// supabaseClient.js
+const { createClient } =require('@supabase/supabase-js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
